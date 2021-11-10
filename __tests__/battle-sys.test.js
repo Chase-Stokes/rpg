52c8703c65d72df1693f2ef67c539df14f3c72e5
Chase-Stokes <chasehstokes@gmail.com>
2021-11-10 11:33:10 -0800

    test battle function
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Anna Pittman <annaepittman@gmail.com>

--- __tests__/battle-sys.test.js
+++ __tests__/battle-sys.test.js
@@ -1,11 +1,20 @@
 import { exportAllDeclaration } from '@babel/types';
 import { roll } from './../src/battle-sys.js';
+import { battle } from './../src/battle-sys.js';
 
 describe("roll", () => {
 
   test("should roll a random number between 1 and 20", () => {
-    const roll1 = roll();
-    expect(roll1).toBeLessThanOrEqual(20);
+    const roll1 = roll(2);
+    expect(roll1).toBeLessThanOrEqual(22);
     expect(roll1).toBeGreaterThan(0);
   });
 })
+
+describe("battle", () => {
+
+  test("should compare two number variables and return true if first argument is higher", () => {
+    expect(battle(15, 13)).toEqual(true)
+  });
+})
+
--- src/battle-sys.js
+++ src/battle-sys.js
@@ -1,6 +1,10 @@
 import Character from './../src/char-creation.js';
 
-export function roll(){
+export function roll(stat){
    let roll1 = Math.floor(Math.random() * 20 ) + 1;
-  return roll1;
+  return roll1 += stat;
 } 
+
+export function battle(playerRoll, enemyRoll) {
+  return playerRoll > enemyRoll;
+}
